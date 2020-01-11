beed0287aa5f8333929d8c34610dbe16e4095faf
Ca#9874123 <cassiointw1993@gmail.com>
2020-01-11 20:16:06 -0300

    ADD[Ninjaflix]-configuration browser run

--- ninjaflix/features/support/env.rb
+++ ninjaflix/features/support/env.rb
@@ -10,9 +10,25 @@ World(Helpers)
 # carregamento do arquivo de ambiente
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
 
+@browser = ENV["BROWSER"]
+
+# carregamento navegadores
+case @browser
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "chromeHeadless"
+  @driver = :selenium_chrome_headless
+when "firefoxHeadless"
+  @driver = :selenium_headless
+else
+  puts "Invalid Browser"
+end
+
 Capybara.configure do |config|
   # configuração do driver (browser) que irá executar os testes
-  config.default_driver = :selenium_chrome
+  config.default_driver = @driver
 
   # host padrão para a aplicação
   config.app_host = CONFIG["url"]
